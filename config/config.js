dbaf309f918c044f3ef328072be16232832bdc82
almas615 <almasadlilwafi615@gmail.com>
2022-01-25 15:55:21 +0700

    edit config production

--- config/config.js
+++ config/config.js
@@ -24,10 +24,10 @@ module.exports = {
     "dialect": "postgres"
   },
   production: {
-    "username": "ulul",
-    "password": "canggih",
-    "database": "database_ch9",
-    "host": "127.0.0.1",
+    "username": "oitouxpfzxlcun",
+    "password": "00fc1d83879a30392591c57c49f6402c93481fcf9c0ec9fac1bb016e21c2dc59",
+    "database": "dep4g4kolur1t5",
+    "host": "ec2-44-199-52-133.compute-1.amazonaws.com",
     "dialect": "postgres"
   },
 };
