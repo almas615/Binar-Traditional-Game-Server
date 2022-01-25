47cc96e75edf69af40afb72e4a8f6ec96bcf4f65
almas615 <almasadlilwafi615@gmail.com>
2022-01-25 17:23:03 +0700

    edit

--- config/config.js
+++ config/config.js
@@ -22,12 +22,8 @@ module.exports = {
     "database": "database_ch9",
     "host": "127.0.0.1",
     "dialect": "postgres"
-  }
-  // production: {
-  //   "username": "ulul",
-  //   "password": "canggih",
-  //   "database": "database_ch9",
-  //   "host": "127.0.0.1",
-  //   "dialect": "postgres"
-  // },
+  },
+  production: {
+    "use_env_variable": "DATABASE_URL",
+  },
 };
